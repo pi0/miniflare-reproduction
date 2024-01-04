0167b8b980437f13bfd1d0b9fbcce2d1daf2aed1
Pooya Parsa <pooya@pi0.io>
2024-01-04 18:54:31 +0100

    comment second

--- src/worker.mjs
+++ src/worker.mjs
@@ -7,8 +7,8 @@ b = await import("./mod-b.mjs").then((r) => r.sum(3, 4));
 export default {
   async fetch(request, env, ctx) {
     // This works (also in parallel)
-    a = await import("./mod-a.mjs").then((r) => r.sum(1, 2));
-    b = await import("./mod-b.mjs").then((r) => r.sum(3, 4));
+    // a = await import("./mod-a.mjs").then((r) => r.sum(1, 2));
+    // b = await import("./mod-b.mjs").then((r) => r.sum(3, 4));
 
     return new Response(JSON.stringify([a, b], null, 2), {
       headers: { "content-type": "application/json" },
